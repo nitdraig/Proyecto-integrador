8d7f8212b985d1e6d2f8b558489910eab2c03549
Agustín Avellaneda <100479647+nitdraig@users.noreply.github.com>
2022-10-16 16:42:17 -0300

    front-summary2

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { EducationComponent } from './components/education/education.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    EducationComponent,
+    NavBarComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    FooterComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
