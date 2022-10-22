a5cea12dbfa9a4c2c49796cb13bc295e8593df6d
Agustín Avellaneda <100479647+nitdraig@users.noreply.github.com>
2022-10-22 14:20:04 -0300

    Back-end2 summary2

---
+++ back-end2/src/test/java/com/draig2/draig2/Draig2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draig2.draig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Draig2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
